e8aefecae7fbd332381bcb781fbf9ed979944e37
Erislandio Silva <erislandio.silva@luizalabs.com>
2020-12-29 23:26:09 -0300

    feat: implementaçao do model de campo

---
+++ src/br/com/cm/exceptions/ExplosaoException.java
@@ -0,0 +1,5 @@
+package br.com.cm.exceptions;
+
+public class ExplosaoException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
--- src/br/com/cm/models/Campo.java
+++ src/br/com/cm/models/Campo.java
@@ -3,6 +3,8 @@ package br.com.cm.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cm.exceptions.ExplosaoException;
+
 public class Campo {
 
 	private final int linha;
@@ -12,11 +14,16 @@ public class Campo {
 	private boolean aberto = false;
 	private boolean marcado = false;
 
-	private List<Campo> vizinhos = new ArrayList<Campo>();
+	private List<Campo> vizinhos = new ArrayList<>();
 
 	public Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
+	}
+
+	private boolean add(Campo campo) {
+		this.vizinhos.add(campo);
+		return true;
 	}
 
 	public boolean adicionarVizinhos(Campo vizinho) {
@@ -29,13 +36,82 @@ public class Campo {
 		int deltaGeral = deltaColuna + deltaLinha;
 
 		if (deltaGeral == 1 && !diagonal) {
-			this.vizinhos.add(vizinho);
-			return true;
+			return this.add(vizinho);
 		} else if (deltaGeral == 2 && diagonal) {
-			this.vizinhos.add(vizinho);
-			return true;
+			return this.add(vizinho);
 		} else {
 			return false;
 		}
+	}
+
+	public void alternarMarcacao() {
+		if (!this.aberto) {
+			this.marcado = !this.marcado;
+		}
+	}
+
+	public boolean abrir() {
+
+		if (this.aberto && !this.marcado) {
+			this.aberto = true;
+
+			if (this.minado) {
+				throw new ExplosaoException();
+			}
+
+			if (this.vizinhacaSegura()) {
+				this.vizinhos.forEach(vizinho -> vizinho.abrir());
+			}
+
+			return true;
+
+		} else {
+			return false;
+		}
+
+	}
+
+	public void minar() {
+		this.minado = true;
+	}
+
+	public boolean isAberto() {
+		return this.aberto;
+	}
+
+	public boolean vizinhacaSegura() {
+		return this.vizinhos.stream().allMatch(vizinho -> !vizinho.minado);
+	}
+
+	public boolean isMarcado() {
+		return this.marcado;
+	}
+
+	public boolean isFechado() {
+		return !this.isAberto();
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public boolean isMinado() {
+		return minado;
+	}
+
+	public void setMinado(boolean minado) {
+		this.minado = minado;
+	}
+
+	public void setAberto(boolean aberto) {
+		this.aberto = aberto;
+	}
+
+	public void setMarcado(boolean marcado) {
+		this.marcado = marcado;
 	}
 }
